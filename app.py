9fcc6654e48d03412ab4e43ebe4022ea2d570901
Miguel Angel <miguelanghdez@outlook>
2023-12-21 09:48:05 +0000

    Update user authentication logic

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from bankaccount import BankAccount
+import logging
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    my_account = BankAccount("Miguel Ángel")
+    logging.info(my_account)
+    my_account.show_balance()
+    my_account.deposit(200)
+    return render_template('index.html', account=my_account)
+
+if __name__ == '__main__':
+    app.run(debug=True)
